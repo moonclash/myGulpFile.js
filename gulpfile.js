e99a20dc56f31e427117bd0b6156f23dbc8e6e1f
emil <emil.mladenov@dwellant.com>
2017-04-04 17:07:30 +0100

    added javascript ES6 to lower compilation, file minification support and refactored entire file to follow ES6 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,70 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+'use strict';
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const fs = require('fs');
 
-gulp.task('sass',function() {
-  return gulp.src('src/assets/styles/scss/*.scss')
+gulp.task('sass', () =>  {
+  return gulp.src('src/styles/scss/*.scss')
   .pipe(sass())
-  .pipe(gulp.dest('src/assets/styles/css'))
+  .pipe(gulp.dest('src/styles/css'))
   .pipe(browserSync.reload({
     stream : true
   }))
 });
 
 
-gulp.task('autoprefixer',function() {
-  return gulp.src('src/assets/styles/css/*.css')
+gulp.task('autoprefixer',() => {
+  return gulp.src('src/styles/css/*.css')
   .pipe(autoprefixer({
-    browsers : ['last 2 versions'],
+    browsers : ['last 5 versions'],
     cascade : false
   }))
-  .pipe(gulp.dest('src/assets/styles/css'));
+  .pipe(gulp.dest('src/styles/css'));
 });
 
-gulp.task('browserSync',function() {
+gulp.task('browserSync', () => {
   browserSync.init({
     server: {
-      baseDir: 'src'
+        baseDir: 'src',
+        socket: {
+            domain: 'dwellant.dev:8085'
+        }
     },
   })
+});
+
+gulp.task('cssQuality', () =>  {
+  var regex = /(\w+{)|:+\w{1,50}/gi;
+  fs.readFile('src/styles/scss/site.scss','utf8',(err,data) => {
+    var file = data;
+    if (file.match(regex)) {
+      throw new Error(`Please refactor the code quality of the following pieces: ${file.match(regex)}`)
+    }
+  });
+});
+
+gulp.task('babelify', () => {
+  return gulp.src('src/scripts/*.js')
+  .pipe(babel({
+    presets: ['es2015']
+  }))
+  .pipe(gulp.dest('src/scripts/es5'));
+});
+
+gulp.task('uglify', () => {
+  return gulp.src('src/scripts/es5/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('src/scripts/es5/min'));
 })
 
 
-gulp.task('watch',['browserSync'],function() {
-  gulp.watch('src/assets/styles/scss/*.scss',['sass']);
-  gulp.watch('src/assets/styles/css/*.css',['autoprefixer']);
-  gulp.watch('src/*.html',browserSync.reload);
-  gulp.watch('src/assets/scripts/*.js',browserSync.reload)
+gulp.task('watch',['browserSync'], () =>  {
+  gulp.watch('src/styles/scss/*.scss',['sass']);
+  gulp.watch('src/styles/css/*.css',['autoprefixer']);
+  gulp.watch('src/*.html', browserSync.reload);
+  gulp.watch('src/scripts/*.js',['babelify','uglify',browserSync.reload]);
 });
-
